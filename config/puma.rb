d6590e32d8f0621b99cc347dacf33c242b259845
Michiel Sikkes <michiel@firmhouse.com>
2016-08-29 12:16:22 +0200

    Use static site and buildpack

--- config/puma.rb
+++
@@ -1,5 +0,0 @@
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
-threads threads_count, threads_count
-port        ENV.fetch("PORT") { 3000 }
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-preload_app!
